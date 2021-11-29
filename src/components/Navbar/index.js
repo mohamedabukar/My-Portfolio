142bc1d203d8e1493dbeff384159a264cc98eee1
Mohamed <mohamed.h.abukar@gmail.com>
2021-11-28 19:44:31 -0600

    phone number and email added.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./style.css";
+import {Phone, Mail} from "@material-ui/icons"
 
 export default function Navbar(){
     return(
@@ -8,13 +9,17 @@ export default function Navbar(){
                 <div className="wrapper">
                     <div className="left">
                         <a href="#bio" className="logo">Bio.</a>
-                        <a href="#apps">Apps</a>
                         <div className="itemContainer">
-                            
+                            <Phone className="icon"/>
+                            <span>612-644-0078</span>
+                        </div>
+                        <div className="itemContainer">
+                            <Mail className="icon"/>
+                            <span>mohamed.h.abukar@gmail.com</span>
                         </div>
                     </div>
                     <div className="right">
-                        This is the right side
+
                     </div>
                 </div>
             </div>
