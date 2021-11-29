f2b3cbddd201f53233f0bfce3a440d22cb8dd929
Mohamed <mohamed.h.abukar@gmail.com>
2021-11-28 22:52:21 -0600

    image added to bio.

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./style.css";
-
+import fire from "../../Images/campfire.png";
 export default function Bio(){
     return(
         <>
-            <div className="bio" id="bio">
-                
+            <div className="bio">
+                <div className="container">
+                    <h1 className="hello">Hello</h1>
+                    <h1>Welcome to my website</h1>
+                </div>
+                <img className="fire" src={fire} alt="fire"></img>
             </div>
         </>
     );
