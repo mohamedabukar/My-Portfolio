42c352dd7179f5744763bb48455644d8ba674f91
Mohamed <mohamed.h.abukar@gmail.com>
2021-11-28 20:34:29 -0600

    menu set.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./style.css";
 import {Phone, Mail} from "@material-ui/icons"
 
-export default function Navbar(){
+export default function Navbar({setMenuOpen, menuOpen}){
     return(
         <>
-            <div className="navbar active">
+            <div className={"navbar " + (menuOpen && "active")}>
                 <div className="wrapper">
                     <div className="left">
                         <a href="#bio" className="logo">Bio.</a>
@@ -19,7 +19,7 @@ export default function Navbar(){
                         </div>
                     </div>
                     <div className="right">
-                        <div className="hamburger">
+                        <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
                             <span className="line1"></span>
                             <span className="line2"></span>
                             <span className="line3"></span>
