ee94531ba483b2d9fadf90e7d5d69814ee0236e6
Mohamed <mohamed.h.abukar@gmail.com>
2021-11-29 20:13:59 -0600

    image added.

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./style.css";
 import fire from "../../Images/campfire.png";
+import profile from "../../Images/me.jpg";
 export default function Bio(){
     return(
         <>
             <div className="bio">
                 <div className="container">
-                    <h1 className="hello">Hello</h1>
-                    <h1>Welcome to my website</h1>
+                    <h1 className="name">Mohamed Abukar</h1>
                 </div>
+                <img className="profile" src={profile} alt="profile"></img>
                 <img className="fire" src={fire} alt="fire"></img>
             </div>
         </>
