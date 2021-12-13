4a80d07e4be4a159118a84b1224bc8bc32097339
Mohamed <mohamed.h.abukar@gmail.com>
2021-12-13 17:11:33 -0600

    contact links and email added on page.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Footer from "../footer";
 import "./style.css";
 
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <>
             <div className="contact" id="contact">
-                
+                <h1>Contact</h1>
+                <div className="form">
+                    <h2><strong>Email:        </strong>mohamed.h.abukar@gmail.com</h2>
+                    <h2><strong>Github:</strong><a href="https://github.com/mohamedabukar" target="_blank" rel="noreferrer"> My Github</a></h2>
+                    <h2><strong>My phone number:  </strong> 612-644-0078</h2>
+                    <h2><strong>linkdin: </strong><a href="https://www.linkedin.com/in/mohamed-abukar-0a99b2211/" target="_blank" rel="noreferrer">My Linkdin</a></h2>
+                </div>
+                <Footer />
             </div>
         </>
     );
