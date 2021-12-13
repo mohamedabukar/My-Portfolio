9ee9d189f17ec6c7c210bbd2b43b58a99c749297
Mohamed <mohamed.h.abukar@gmail.com>
2021-12-13 17:07:46 -0600

    text added to page.

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -1,16 +1,31 @@
 import React from "react";
+import Footer from "../footer";
 import "./style.css";
-import fire from "../../Images/campfire.png";
-import profile from "../../Images/me.jpg";
-export default function Bio(){
-    return(
+// import fire from "../../Images/campfire.png";
+import profile from "../../Images/profile.jpg";
+export default function Bio() {
+    return (
         <>
-            <div className="bio">
-                <div className="container">
-                    <h1 className="name">Mohamed Abukar</h1>
+            <div className="bio" id="home">
+                <div>
+                    <h1 className="center">About Me</h1>
+                    <div className="container">
+                        <div className="text">
+                            <h2>Hello! My name is Mohamed Abukar</h2>
+                            <p>I am a Full Stack Web Developer from Apple Valley, MN. I became a Web Developer by taking a bootcamp over the summer of 2021.</p>
+                            <h2>Skills</h2>
+                            <p>HTML, CSS, Javascript, JQuery, React, NodeJS, Express, MySQL, Sequelize, MongoDB, Mongoose, REST API's, GraphQL.</p>
+                            <h2>Hobbies</h2>
+                            <p>Playing Basketball, gaming, spending time with family, learning new skills.</p>
+                        </div>
+                        <div className="photo">
+                            <img className="profile" src={profile} alt="profile" height="400px" width="450px"></img>
+                        </div>
+                    </div>
                 </div>
-                <img className="profile" src={profile} alt="profile"></img>
-                <img className="fire" src={fire} alt="fire"></img>
+                <Footer />
+
+                {/* <img className="fire" src={fire} alt="fire"></img> */}
             </div>
         </>
     );
