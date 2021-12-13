d1530d936d6fb319a10c69195f166efc2e5f011a
Mohamed <mohamed.h.abukar@gmail.com>
2021-12-13 17:04:59 -0600

    footer added.

---
+++ src/components/footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import github from "../../Images/github.jpg";
+import linkedin from "../../Images/linkdin.jpg";
+import "./style.css";
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer-container text-center">
+               <a href="https://github.com/mohamedabukar" target="_blank" rel="noreferrer"><img src={github} width="50px" height="50px" alt="github" /></a>
+               <a href="https://www.linkedin.com/in/mohamed-h-abukar/" target="_blank" rel="noreferrer"><img src={linkedin} width="50px" height="50px"alt="linkedin"/></a>
+            </div>
+        </footer>
+    );
+}
