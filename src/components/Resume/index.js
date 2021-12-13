f2880dae03ec5126722554bfa6373ab107c2d962
Mohamed <mohamed.h.abukar@gmail.com>
2021-12-13 17:19:39 -0600

    resume added to page.

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,11 +1,36 @@
 import React from "react";
+import Footer from "../footer";
 import "./style.css";
 
-export default function Resume(){
-    return(
+export default function Resume() {
+    return (
         <>
             <div className="resume" id="resume">
-                
+                <h1>Resume</h1>
+                <h2 className="resume">
+                    <a className="resume-link" href="https://docs.google.com/document/d/1RxdfONZ7IiGqhbTwwonCETXpTtNFNtiHvESaIhqF_20/edit?usp=sharing" target="_blank" rel="noreferrer">Click for link to Resume</a>
+                </h2>
+                <h3>Front-end Proficiencies</h3>
+                <ul className="skills">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>jQuery</li>
+                    <li>responsive design</li>
+                    <li>React</li>
+                    <li>Bootstrap</li>
+                </ul>
+                <h3>Back-end Proficiencies</h3>
+                <ul className="skills">
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MySQL, Sequelize</li>
+                    <li>MongoDB, Mongoose</li>
+                    <li>REST</li>
+                    <li>GraphQL</li>
+                </ul>
+                <Footer />
             </div>
         </>
     );
